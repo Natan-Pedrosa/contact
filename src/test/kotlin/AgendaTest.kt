cc8b20fc30dd70bfcd59823626b15013c432ab68
Natan Pedrosa <natanpedrosa123@gmail.com>
2023-05-15 12:28:47 -0300

    Add a project with menu for add, remove, edit, list and count. PhoneNumber has a mask

---
+++ src/test/kotlin/AgendaTest.kt
@@ -0,0 +1,14 @@
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.*
+
+class AgendaTest {
+
+    @Test
+    fun getAgenda() {
+    }
+
+    @Test
+    fun showMenu() {
+    }
+}
